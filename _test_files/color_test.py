d074f2305356d3cf6002dc874901c8ff3f6755aa
Twallace51 <42808466+Twallace51@users.noreply.github.com>
2020-05-16 18:08:25 -0400

    Add files via upload

---
+++ _test_files/color_test.py
@@ -0,0 +1,31 @@
+highlighting for python files using customized config files:
+    my_dark.conf         colorscheme
+    filetypes.python     filetype definition
+
+                usage
+----------------------------------
+default         code statements
+
+'string_1       variables'
+
+"string         right clickable file paths and URLS"
+
+# comment       alert
+
+'''
+multilines      disabled code
+'''
+
+"""
+multilines      code comments
+                SQL statements
+"""
+
+keywords        for in and while etc
+
+keywords2       file type
+
+operators       = < > ) ( * .
+
+' string_eol      ?
+" string_eol
